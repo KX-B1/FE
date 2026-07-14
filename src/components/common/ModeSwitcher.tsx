e42144398d15e9df671bd478031e294e53a9cd08
UJin1122 <enooooo93@gmail.com>
2026-07-14 11:51:16 +0900

    feat: 뷰 모드 전환/훅 연동

--- src/components/common/ModeSwitcher.tsx
+++ src/components/common/ModeSwitcher.tsx
@@ -5,16 +5,13 @@ import { useViewModeStore } from '@/stores/viewModeStore';
 const CanvasArea = dynamic(() => import('@/components/canvas/CanvasArea'), {
   ssr: false,
 });
+const StoryboardArea = dynamic(
+  () => import('@/components/storyboard/StoryboardArea'),
+  { ssr: false }
+);
 
 // 스토리보드/캔버스 분기 영역
 export default function ModeSwitcher() {
   const viewMode = useViewModeStore((state) => state.viewMode);
-  return (
-    <div className="flex-1 h-full">
-      {viewMode === 'canvas' && <CanvasArea />}
-      {/* StoryboardArea 완성되면 아래로 교체
-      {viewMode === 'storyboard' ? <StoryboardArea /> : <CanvasArea />}
-      */}
-    </div>
-  );
+  return viewMode === 'storyboard' ? <StoryboardArea /> : <CanvasArea />;
 }
